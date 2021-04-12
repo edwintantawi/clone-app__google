24c1bfe72b8be9703543ff488fee4d995fc39263
edwintantawi <edwintantawi@gmail.com>
2021-04-12 22:02:43 +0700

    init project, clean up project, added react router, material ui, material icons

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <center>
+        <h1>Let's Build Google Clone</h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
